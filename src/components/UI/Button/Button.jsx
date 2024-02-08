27fa1d9f6457e58f37dd9c9da3ce8e7166e74dc8
Oleksandr <bajmutov@gmail.com>
2024-02-08 14:03:05 +0200

    pad

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -2,12 +2,7 @@ import { StyledButton } from './Button.styled';
 
 const Button = ({ padY = 12, padX = 50, onClick, disabled, children }) => {
   return (
-    <StyledButton
-      $padY={padY}
-      $padX={padX}
-      disabled={disabled}
-      onClick={onClick}
-    >
+    <StyledButton padY={padY} padX={padX} disabled={disabled} onClick={onClick}>
       {children}
     </StyledButton>
   );
