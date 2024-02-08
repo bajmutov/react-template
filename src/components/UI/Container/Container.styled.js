aa1a50b52dc3044edaf683b7d0dbd2fcf3b5ec87
Oleksandr <bajmutov@gmail.com>
2024-02-08 14:05:19 +0200

    media

--- src/components/UI/Container/Container.styled.js
+++ src/components/UI/Container/Container.styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
-  width: 100%;
+  /* width: 100%;  media<378 px*/
+  width: 1240px;
   margin: 0 auto;
   padding: 0 20px;
 `;
